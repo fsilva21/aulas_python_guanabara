7c5a7e0bb55a71b28b6d3ab7c695493f46fb1895
Sombra <fabiosilva2048@gmail.com>
2025-11-19 21:48:03 -0300

    Resolução dos exercícios 105, 106

--- Exercícios/ex105.py
+++ Exercícios/ex105.py
@@ -6,4 +6,24 @@
 # - A situação (opcional)
 # Adicione também as DOCSTRINGS da função
 
-def notas():
+def notas(* n, sit=False):
+    """
+
+    :param n: Insira as notas da turma
+    :param sit: Caso deseje ver a situação: sit=True
+    :return: Retorna o dicionário com os dados da turma e a situação caso solicitado
+    """
+    media = sum(n) / len(n)
+    dicio = {'total': len(n), 'maior': max(n), 'menor': min(n), 'média': f'{media:.2f}'}
+    if sit:
+        if media >= 7:
+            dicio['situação'] = 'BOA'
+        elif media < 5:
+            dicio['situação'] = 'RUIM'
+        else:
+            dicio['situação'] = 'RECUPERAÇÃO'
+    return dicio
+
+
+resp = notas(7.5,7,8.5, sit=True)
+help(notas)
--- Exercícios/ex106.py
+++ Exercícios/ex106.py
@@ -2,3 +2,31 @@
 # O usuário vai digitar o comando e o manual vai aparecer.
 # Quando o usuário digitar a palavra "FIM", o programa se encerrará.
 # OBS: Use cores!
+
+from colorama import init, Fore, Back
+from time import sleep
+init(autoreset=True)
+def titulo(msg):
+    tam = len(msg) + 4
+    print(Back.GREEN + '*' * tam)
+    print(Back.BLACK + f'  {msg}  ')
+    print(Back.GREEN + '*' * tam)
+    sleep(1)
+
+def ajuda(c):
+    help(c)
+    sleep(1)
+
+
+#Programa principal
+while True:
+    pergunta = input(Fore.BLACK + Back.LIGHTBLUE_EX + 'Função ou Biblioteca > ').strip()
+    if pergunta.upper() == 'FIM':
+        f='ENCERRANDO PROGRAMA...'
+        print(Back.RED + '~' * len(f))
+        print(Back.RED + f'{f}')
+        print(Back.RED + '~' * len(f))
+        sleep(1)
+        break
+    titulo(f'Acessando o manual do comando \'{pergunta}\'')
+    ajuda(pergunta)
